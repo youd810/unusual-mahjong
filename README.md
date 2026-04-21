cce7ade453d17b1e54e8a3f187cf6bf9318166ce
Yudi810 <contact.wahyudiarfandi@gmail.com>
2026-04-21 16:14:31 +0800

    yaku checkpoint (35ish more)

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-wip
+# <div style="text-align: center;">WIP</div>
 
 ![majan](https://files.catbox.moe/9ix2ks.png)
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,23 @@ enum Honor {
     South,
 }
 
+#[derive(PartialEq, Eq, Clone)]
+enum Mentsu {
+    Jantou(Vec<Tile>),
+    Koutsu(Vec<Tile>),
+    Shuntsu(Vec<Tile>),
+}
+
+struct Player {
+    points: i32,
+    is_tenpai: bool,
+    is_hand_closed: bool,
+    is_alive: bool,
+    aggression: u8,
+    defense: u8,
+    cheating_inclination: u8, 
+}
+
 fn is_terminal(tile: &Tile) -> bool {
     match tile {
         Tile::Sou(1 | 9) | Tile::Pin(1 | 9) | Tile::Man(1 | 9) => true,
@@ -32,43 +49,111 @@ fn is_yaochuhai(tile: &Tile) -> bool {
     is_terminal(tile) || is_honor(tile)
 }
 
+fn check_win(hand: &Vec<Tile>) -> Option<Vec<Vec<Mentsu>>> {
+    let results = decompose(hand);
+    if results.is_empty() {
+        None
+    } else {
+        Some(results)
+    }
+}
+
 fn tanyao(hand: &Vec<Tile>) -> bool {
+    // add is_closed cond
     hand.iter().all(|x| !is_yaochuhai(x))    
 }
 
-// will deal with this later
-//fn iipeikou(hand: &Vec<Tile>) -> bool {
-//    let grouped = vec![hand[..3].to_vec(), hand[3..6].to_vec(), hand[6..9].to_vec(), hand[9..12].to_vec(), hand[12..].to_vec()];
-//    let mut flag = false;
-//    for i in 0..grouped.len() -1 {
-//        if i == 0 {
-//            continue;
-//        }
-//        if shuntsu(&grouped[i]) && shuntsu(&grouped[i-1]) &&  grouped[i] == grouped[i-1] {
-//            flag = true
-//        }
-//    }
-//    flag
-//}
+fn kokushi_musou(hand: &Vec<Tile>) -> bool {
+    if hand.iter().all(|x| is_yaochuhai(x)) {
+        let mut pair_counter: u8 = 0;
+        for i in 0..hand.len() - 1 {
+            if hand[i] == hand[i+1] {
+                pair_counter += 1;
+            }
+        }
+        if pair_counter == 1 {
+            return true;
+        } 
+        return false;
+    }
+    false
+} 
+        
 
-fn decompose(tiles: &Vec<Tile>) -> Vec<Vec<Vec<Tile>>> {
+fn tsuuisou(hand: &Vec<Tile>) -> bool {
+    hand.iter().all(|x| is_honor(x))
+}
+
+fn iipeikou(results: &Vec<Vec<Mentsu>>) -> bool {
+    results.iter().any(|result| {
+        let shuntsu: Vec<&Mentsu> = result.iter().filter(|x| matches!(x, Mentsu::Shuntsu(_))).collect();
+ 
+        for i in 0..shuntsu.len() {
+            for j in i+1..shuntsu.len() {
+                if shuntsu[i] == shuntsu[j] {
+                    return true;
+                }
+            }
+        }
+        // for any()
+        false
+    })
+}
+
+fn all_tiles() -> Vec<Tile> {
+    // will compare vec vs array later
+    let mut tiles = vec![];
+    for n in 1..=9 {
+        tiles.push(Tile::Man(n));
+        tiles.push(Tile::Pin(n));
+        tiles.push(Tile::Sou(n));
+    }
+    tiles.push(Tile::Honor(Honor::East));
+    tiles.push(Tile::Honor(Honor::South));
+    tiles.push(Tile::Honor(Honor::West));
+    tiles.push(Tile::Honor(Honor::North));
+    tiles.push(Tile::Honor(Honor::White));
+    tiles.push(Tile::Honor(Honor::Green));
+    tiles.push(Tile::Honor(Honor::Red));
+    tiles
+}
+
+fn tenpai(&mut self, hand: &Vec<Tile>) -> Vec<Tile> {
+    let mut waiting_on: Vec<Tile> = vec![];
+    for tile in all_tiles() {
+        let mut hand_speculated = hand.clone();
+        hand_speculated.push(tile);
+        if !decompose(&hand_speculated).is_empty() {
+            if self.is_tenpai == false {
+                self.is_tenpai = true;
+            }
+            waiting_on.push(tile);
+        }
+    }
+    waiting_on
+}
+
+// one hand can return different mentsu varations
+// example: [sou1, sou1, sou1 sou2, sou2, sou2, sou3, sou3, sou3] 
+// can return [shuntsu, shuntsu, shuntsu] or [koutsu, koutsu, koutsu]
+// so the final result is a vector of those two
+fn decompose(tiles: &Vec<Tile>) -> Vec<Vec<Mentsu>> {
     let mut results = vec![];
-    let mut sorted = tiles.clone();
-    sorted.sort();
 
-    
-
-    for i in 0..sorted.len() - 1 {
-        if sorted[i] == sorted[i+1] {
-            let pair = vec![sorted[i].clone(), sorted[i+1].clone()];
-            let mut remaining = sorted.clone();
+    for i in 0..tiles.len() - 1 {
+        if tiles[i] == tiles[i+1] {
+            if i > 0 && tiles[i] == tiles[i-1]{
+                continue;
+            }
+            let pair = Mentsu::Jantou(vec![tiles[i].clone(), tiles[i+1].clone()]);
+            let mut remaining = tiles.clone();
+            // removes jantou from mentsu check
             remaining.remove(i + 1);
             remaining.remove(i);
 
             find_mentsu(&remaining, vec![pair], &mut results);
         }
     }
-
     results
 }
 
@@ -82,7 +167,7 @@ fn next_tile_sequence(tile: &Tile) -> Option<Tile> {
 }
 
 
-fn find_mentsu(remaining: &Vec<Tile>, current: Vec<Vec<Tile>>, results: &mut Vec<Vec<Vec<Tile>>>) {
+fn find_mentsu(remaining: &Vec<Tile>, current: Vec<Mentsu>, results: &mut Vec<Vec<Mentsu>>) {
     if remaining.is_empty() {
         results.push(current);
         return;
@@ -90,7 +175,7 @@ fn find_mentsu(remaining: &Vec<Tile>, current: Vec<Vec<Tile>>, results: &mut Vec
 
     // koutsu check
     if remaining.len() >= 3 && remaining[0] == remaining[1] && remaining[0] == remaining[2] {
-        let koutsu_group = vec![remaining[0].clone(), remaining[1].clone(), remaining[2].clone()];
+        let koutsu_group = Mentsu::Koutsu(vec![remaining[0].clone(), remaining[1].clone(), remaining[2].clone()]);
         let mut new_remaining = remaining.clone();
         for _ in 0..3 {
             new_remaining.remove(0);
@@ -105,7 +190,7 @@ fn find_mentsu(remaining: &Vec<Tile>, current: Vec<Vec<Tile>>, results: &mut Vec
         if let Some(third) = next_tile_sequence(&second) {
             if let Some(second_seq) = remaining.iter().skip(1).position(|x| *x == second).map(|i| i + 1) {
                 if let Some(third_seq) = remaining.iter().skip(second_seq + 1).position(|x| *x == third).map(|i| i + second_seq + 1) {
-                    let shuntsu_group = vec![remaining[0].clone(), remaining[second_seq].clone(), remaining[third_seq].clone()];
+                    let shuntsu_group = Mentsu::Shuntsu(vec![remaining[0].clone(), remaining[second_seq].clone(), remaining[third_seq].clone()]);
                     let mut new_remaining = remaining.clone();
                     // starts from the highest index
                     for idx in vec![third_seq, second_seq, 0] {
@@ -118,10 +203,32 @@ fn find_mentsu(remaining: &Vec<Tile>, current: Vec<Vec<Tile>>, results: &mut Vec
             }
         }
     }
+
+    // jantou check here
+
 }
+
+
 
 fn main() {
     let mut wall = vec![Tile::Sou(1), Tile::Honor(Honor::Red)];
+
+    // logical sorting when player picks up a card
+    let mut hand = vec![Tile::Sou(1), Tile::Honor(Honor::Red)];
+    hand.sort();
+    if let Some(results) = check_win(&hand) {
+        if iipeikou(&results) {}  
+        if tanyao(&hand) {} 
+        if tsuuisou(&hand){}
+        if daisangen(&hand){}
+        if toitoi(&results) {}
+        if honitsu(&hand) {}
+        if  {} // other yaku
+    
+    } else if { // special yaku like kokushi musou or chiitoitsu
+
+    }
+
 
     println!("{}", is_terminal(&wall[0]));
 }
